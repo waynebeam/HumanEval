cf548b61c16eacb2e62924c4a139f9932246b363
waynebeam <waynejcbeam@me.com>
2022-06-29 08:37:05 -0400

    Update runs at 60fps on the eval bar with the wiggle added

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
+import math
+
 from kivy.app import App
 from kivy.graphics import Color, Rectangle, Line
+from kivy.metrics import dp
 from kivy.properties import StringProperty, ObjectProperty, NumericProperty
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.widget import Widget
+from kivy.clock import Clock
 
 
 class HumanEval(App):
-    pass
+    def on_start(self):
+        Clock.schedule_interval(self.root.ids.eval_bar.update, 1 / 60)
 
 
 class EvalBar(Widget):
     current_eval = NumericProperty(0)
+    wiggle = NumericProperty(0)
+    wiggle_height = dp(5)
     color = Color(rgb=(1, 1, 1))
     rect = Rectangle
+    elapsed_time = 0
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    def update(self, dt):
+        self.wiggle = self.wiggle_height * math.sin(self.elapsed_time)
+        self.elapsed_time += 2 * dt
 
-    def draw_bar(self, eval_out_of_100):
-        half_height = self.height / 2
-        eval_as_decimal = eval_out_of_100 / 100
-        target_height = half_height + (half_height * eval_as_decimal)
-        with self.canvas.before:
-            Color(self.color)
-            self.rect = Rectangle(pos=self.pos, size=(self.width, target_height))
+    # def draw_bar(self, eval_out_of_100):
+    #     half_height = self.height / 2
+    #     eval_as_decimal = eval_out_of_100 / 100
+    #     target_height = half_height + (half_height * eval_as_decimal)
+    #     with self.canvas.before:
+    #         Color(self.color)
+    #         self.rect = Rectangle(pos=self.pos, size=(self.width, target_height))
 
     def set_eval(self, new_eval_out_of_100):
         self.current_eval = new_eval_out_of_100
