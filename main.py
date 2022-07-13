9f5c3c44fce9dd5b300e278d58ff6d2b3226d0b0
waynebeam <waynejcbeam@me.com>
2022-07-13 14:53:51 -0400

    Reworked the set up. description, eval, and symbol are now all group in 1 array when the EvalButtonGrid is set up in set_player_color. All three pieces are put together only there and then used. So one old function removed.

--- main.py
+++ main.py
@@ -146,52 +146,45 @@ class Bubble(Widget):
         self.ellipse.size = self.size
 
 
-def setup_eval_to_word_pairs(color_name: str, eval_bar: EvalBar):
-    evaluation_to_words = [(f"{color_name} is absolutely crushing", .99), (f"{color_name} is winning", .75),
-                           (f"{color_name} is much better", .6), (f"{color_name} is slightly better", .4),
-                           (f"{color_name} is pulling", .25), (f"{color_name} is okay", .1)]
-
-    if color_name == "White":
-        eval_bar.eval_to_symbol[.99] = "!!"
-        eval_bar.eval_to_symbol[.75] = "+-"
-        eval_bar.eval_to_symbol[.6] = "+ \n-"
-        eval_bar.eval_to_symbol[.4] = "+\n="
-        eval_bar.eval_to_symbol[.25] = "+\n="
-        eval_bar.eval_to_symbol[.1] = "="
-    if color_name == "Black":
-        eval_bar.eval_to_symbol[-.99] = "!!"
-        eval_bar.eval_to_symbol[-.75] = "-+"
-        eval_bar.eval_to_symbol[-.6] = "- \n+"
-        eval_bar.eval_to_symbol[-.4] = "-\n="
-        eval_bar.eval_to_symbol[-.25] = "-\n="
-        eval_bar.eval_to_symbol[-.1] = "="
-
-    return evaluation_to_words
-
-
 class EvalButtonGrid(GridLayout):
     player_color = StringProperty()
-    button_bindings = {Button: (str, float)}
+    button_bindings = {Button: (str, float, str)}
     evaluations = []
     direction = 1
     eval_bar = ObjectProperty(EvalBar)
 
+    # noinspection PyStatementEffect
     def set_player_color(self, color_name):
-        if color_name != "White":
+        evaluation_to_words = [str, float, str]
+        if color_name == "White":
+            self.direction = 1
+            evaluation_to_words = [(f"{color_name} is absolutely crushing", .99, "!!"),
+                                   (f"{color_name} is winning", .75, "+-"),
+                                   (f"{color_name} is much better", .6, "+ \n-"),
+                                   (f"{color_name} is slightly better", .4, "+="),
+                                   (f"{color_name} is pulling", .25, "+\n="), (f"{color_name} is okay", .1, "=")]
+        if color_name == "Black":
             self.direction = -1
-        self.evaluations = setup_eval_to_word_pairs(color_name, self.eval_bar)
+            evaluation_to_words = [(f"{color_name} is absolutely crushing", -.99, "-!!"),
+                                   (f"{color_name} is winning", -.75, "-+"),
+                                   (f"{color_name} is much better", -.6, "- \n+"),
+                                   (f"{color_name} is slightly better", -.4, "-="),
+                                   (f"{color_name} is pulling", -.25, "-\n="), (f"{color_name} is okay", -.1, "=")]
+
+        self.evaluations = evaluation_to_words
         for evaluation in self.evaluations:
             btn = Button(text=evaluation[0], font_size=dp(20))
             self.button_bindings[btn] = evaluation
             btn.bind(on_release=self.set_evaluation)
             self.add_widget(btn)
 
+            self.eval_bar.eval_to_symbol[evaluation[1]] = evaluation[2]
+
         return color_name
 
     def set_evaluation(self, btn):
         new_eval_out_of_100 = self.button_bindings[btn][1]
-        if self.player_color == "Black":
-            new_eval_out_of_100 *= -1
+
         self.eval_bar.set_eval(new_eval_out_of_100)
 
 
